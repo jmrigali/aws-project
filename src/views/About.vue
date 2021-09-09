9b029f29fc5a9b338a38fb46291ec5f96afd025e
jmrigali <jodie@backboneplm.com>
2021-09-09 16:47:57 -0600

    commiting deleted branches

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="about">
     <h1>This is Server Information</h1>
-    <h2>{{info.data}}</h2>
+    <h2>{{ info.data }}</h2>
   </div>
 </template>
 <script>
-
-import axios from 'axios';
-export default {
-  data(){
-    return {
-      info: ''
-    }
-  },
-  mounted() {
-    if(this.$store.state.user) {
-      const jwt = this.$store.state.user
-      .getSignInUserSession()
-      .getIdToken()
-      .getJwtToken();
-
-      const config = {
-        headers: {
-          authorization: jwt
-        }
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        info: ''
       }
+    },
+    mounted() {
+      if (this.$store.state.user) {
+        const jwt = this.$store.state.user
+          .getSignInUserSession()
+          .getIdToken()
+          .getJwtToken()
 
-      axios.get('https://r29pm52lfc.execute-api.us-west-2.amazonaws.com/v1/m', config)
-      .then(val => this.info = val)
-      .catch(err => console.log(err))
+        const config = {
+          headers: {
+            authorization: jwt
+          }
+        }
 
-
+        axios
+          .get('https://nuaiyhbfbl.execute-api.us-east-1.amazonaws.com/v1/m', config)
+          .then((val) => (this.info = val))
+          .catch((err) => console.log(err))
+      }
     }
   }
-}
 </script>
